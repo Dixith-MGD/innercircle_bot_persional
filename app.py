775d4db84f63b6b55af4988d6cec64f2662792fc
Dixith-MGD <93250911+Dixith-MGD@users.noreply.github.com>
2023-08-13 11:08:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+from dotenv import load_dotenv
+import pickle
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+import os
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+st.title('INNERCIRCLE BOT')
+st.sidebar.title('Developed by UNORTHODOX')
+st.sidebar.info('Chat')
+pdf_path = 'dataset.pdf'
+pdf_reader = PdfReader(pdf_path)
+text = ""
+for page in pdf_reader.pages:
+    text += page.extract_text()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200,
+    length_function=len
+)
+chunks = text_splitter.split_text(text=text)
+
+if os.path.exists(f"{pdf_path[:-4]}.pkl"):
+    with open(f"{pdf_path[:-4]}.pkl", "rb") as f:
+        VectorStore = pickle.load(f)
+else:
+    embeddings = OpenAIEmbeddings()
+    VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+    with open(f"{pdf_path[:-4]}.pkl", "wb") as f:
+        pickle.dump(VectorStore, f)
+
+query = st.text_input("Ask a question about Innercircle")
+
+if query:
+    embeddings = OpenAIEmbeddings()
+    docs = VectorStore.similarity_search(query=query, k=3)
+    llm = OpenAI()
+    chain = load_qa_chain(llm=llm, chain_type="stuff")
+    with get_openai_callback() as cb:
+        response = chain.run(input_documents=docs, question=query)
+    st.write(response)
